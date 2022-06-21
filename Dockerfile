e493433a735389f5be48878bf018e74998c55d4f
Vitaliy Vasilenko <me@vitalvas.com>
2022-06-21 18:38:20 +0300

    refactor build

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,7 @@
 FROM ubuntu:focal
 
-RUN apt update -qy && \
-    apt install -qy git openssh-server python3-pip curl && \
-    curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg && \
-    echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest focal main" > /etc/apt/sources.list.d/salt.list && \
-    apt update -qy && \
-    apt install -qy salt-master salt-minion salt-ssh salt-syndic salt-cloud salt-api && \
-    pip3 install --no-cache-dir redis M2Crypto pycrypto psycopg-binary hvac gitpython pygit2
-
+COPY build.sh /build.sh
+RUN bash -x /build.sh
 ADD bin /opt/bin/
-
 
 CMD ["/usr/local/bin/salt-master"]
---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+apt update -qy
+apt install -qy git openssh-server python3-pip curl
+
+if [ ! -f  "/usr/sbin/dpkg-split" ]; then
+    ln -s /usr/bin/dpkg-split /usr/sbin/dpkg-split
+fi
+
+if [ ! -f "/usr/sbin/dpkg-deb" ]; then
+    ln -s /usr/bin/dpkg-deb /usr/sbin/dpkg-deb
+fi
+
+curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest/salt-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/latest focal main" > /etc/apt/sources.list.d/salt.list
+apt update -qy
+
+apt install -qy salt-master salt-minion salt-ssh salt-syndic salt-cloud salt-api
+
+pip3 install --no-cache-dir redis M2Crypto pycrypto psycopg-binary hvac gitpython pygit2
