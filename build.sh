f5a9f0b05e7e9c97a69405901ffcb3a4c4318963
Vitaliy Vasylenko <me@vitalvas.com>
2024-07-29 03:36:01 -0400

    feat: update to 3007 version

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ export DEBIAN_FRONTEND=noninteractive
 export USE_STATIC_REQUIREMENTS=1
 
 apt update -qy
-apt install -qy git openssh-server python3-pip curl dpkg swig libssl-dev openssl libgit2-dev python-is-python3 libgit2-dev libffi-dev python3-dev python3-cffi libxslt1-dev \
-    python3-requests python3-requests-oauthlib patchelf
+apt install -qy git openssh-server curl dpkg swig libssl-dev openssl libgit2-dev libffi-dev libgit2-dev libxslt1-dev  patchelf \
+    python-is-python3 python3-pip python3-dev python3-cffi 
 
 if [ ! -f  "/usr/sbin/dpkg-split" ]; then
     ln -s /usr/bin/dpkg-split /usr/sbin/dpkg-split
@@ -18,10 +18,10 @@ if [ ! -f "/usr/sbin/dpkg-deb" ]; then
 fi
 
 curl -fsSL -o /usr/share/keyrings/salt-pubkey-amd64.gpg https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg
-echo "deb [signed-by=/usr/share/keyrings/salt-pubkey-amd64.gpg arch=amd64] https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/3006 jammy main" > /etc/apt/sources.list.d/salt-amd64.list
+echo "deb [signed-by=/usr/share/keyrings/salt-pubkey-amd64.gpg arch=amd64] https://repo.saltproject.io/salt/py3/ubuntu/22.04/amd64/3007 jammy main" > /etc/apt/sources.list.d/salt-amd64.list
 
 curl -fsSL -o /usr/share/keyrings/salt-pubkey-arm64.gpg https://repo.saltproject.io/salt/py3/ubuntu/22.04/arm64/SALT-PROJECT-GPG-PUBKEY-2023.gpg
-echo "deb [signed-by=/usr/share/keyrings/salt-pubkey-arm64.gpg arch=arm64] https://repo.saltproject.io/salt/py3/ubuntu/22.04/arm64/3006 jammy main" > /etc/apt/sources.list.d/salt-arm64.list
+echo "deb [signed-by=/usr/share/keyrings/salt-pubkey-arm64.gpg arch=arm64] https://repo.saltproject.io/salt/py3/ubuntu/22.04/arm64/3007 jammy main" > /etc/apt/sources.list.d/salt-arm64.list
 
 apt update -qy
 
